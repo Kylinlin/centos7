9be3141e651e90a9e99ef715ca27abc1ef82ab32
kylinlin <kylinlingh@foxmail.com>
2015-09-26 09:11:20 +0800

    Update

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -18,8 +18,18 @@ function Startup {
     sh install_lmap.sh
     echo -e "\e[42;35m\e[5mRunning this script: install_others.sh\e[0m" 
     sh install_others.sh
-    echo -e "\e[42;35m\e[5mRunning this script: configure_rsyslog_for_client.sh\e[0m" 
-    sh configure_rsyslog_for_client.sh
+    
+    echo -n -e "\e[1;35mEnter 1 for rsyslog server, 2 for rsyslog client: \e[0m "
+    read RSYSLOG
+    if [[ $RSYSLOG == 1 ]]; then
+        echo -e "\e[42;35m\e[5mRunning this script: configure_rsyslog_for_server.sh\e[0m" 
+        sh configure_rsyslog_for_server.sh
+    elif [[ $RSYSLOG == 2 ]]; then
+        echo -e "\e[42;35m\e[5mRunning this script: configure_rsyslog_for_client.sh\e[0m" 
+        sh configure_rsyslog_for_client.sh
+    else
+        echo -e "\e[1;31mWrong input. \e[0m"
+    fi
     echo -e "\e[42;35m\e[5mConfiguration, you have get it all done.\e[0m" 
 }
 
